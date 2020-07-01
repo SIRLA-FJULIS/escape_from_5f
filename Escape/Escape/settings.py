b8b14891b702958a3fe9b7ceb5af7702ad50891e
Lamuyang <lamuyangyang@gmail.com>
2020-07-02 01:53:08 +0800

    [+] ADD admin

--- Escape/Escape/settings.py
+++ Escape/Escape/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=1(jc(f36jc0z^x&=r*kt3%%w_j#5t(w&0-_wggh_i!_(lfw7m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

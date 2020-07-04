536b159eaaaf12dda61930786a6f24613fb19c57
Lamuyang <lamuyangyang@gmail.com>
2020-07-04 15:49:35 +0800

    [+] ADD user_table

--- Escape/Escape_story/models.py
+++ Escape/Escape_story/models.py
@@ -4,3 +4,6 @@ class Flag(models.Model):
     ans = models.TextField(null=True)
     checkpoint = models.TextField(null=True)
 
+class User_table(models.Model):
+    user = models.CharField(max_length=100,null=True)
+    password = models.CharField(max_length=100,null=True)

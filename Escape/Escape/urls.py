d689d68b9b651d6169b08dcf2ba59953cd3f04aa
Lamuyang <lamuyangyang@gmail.com>
2020-07-05 02:22:40 +0800

    can't fix transweb problem

--- Escape/Escape/urls.py
+++ Escape/Escape/urls.py
@@ -6,7 +6,7 @@ from Escape_story.views import Beginning,Five_A,Toilet,Corridor_I,Corridor_II,De
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Beginning),
-    path('5A/',Five_A),
+    path('5A/',Five_A), ##NUE=
     path('corridor_i/',Corridor_I),
     path('corridor_ii/',Corridor_II),
     path('department_office/',Department_office),

24b2b9fe7b717d84ea455fa1f9eb42de1c6a849f
Lamuyang <lamuyangyang@gmail.com>
2020-07-04 13:23:25 +0800

    [+] ADD templates

--- Escape/Escape/urls.py
+++ Escape/Escape/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from Escape_story.views import home
+from Escape_story.views import Beginning,Five_A,Toilet,Corridor_I,Corridor_II,Department_office
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index),
+    path('', Beginning),
+    path('5A/',Five_A),
+    path('corridor_i',Corridor_I),
+    path('corridor_ii',Corridor_II),
+    path('department_office',Department_office),
+    path('toilet',Toilet),
 ]
